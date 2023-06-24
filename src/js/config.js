3b68ced9c84ea3e7e19e7f43af4dcf1a00bcdf64
nitesh sah <niteshsah671@gmail.com>
2023-06-24 12:20:46 +0530

    documentation added for config.js file which is configuration parameter

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,25 @@
+/**
+ * @param API_URL, URL of the API where feture like search and upload recipy present.
+ */
+
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+/**
+ * @param TIMEOUT_SEC, this is used for to terminate the featcing when it exceeded the certain time. here time is 10sec
+ */
 export const TIMEOUT_SEC = 10;
+
+/**
+ * @param RES_PER_PAGE, this is used for restrict the no of recipe view in the result view, to avoid all result view in single page. here value is 10 per page
+ */
 export const RES_PER_PAGE = 10;
+
+/**
+ * @param KEY, API key for assing all the feture provided by the API
+ */
 export const KEY = '77979f19-68e5-41e7-9735-c2a7ffdc33e2';
+
+/**
+ * @param MODAL_CLOSE_SEC, closing time for the successfull upload message of recipe
+ */
 export const MODAL_CLOSE_SEC = 2.5;
